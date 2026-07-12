fa3a0cdd2680ed31e498053064e55925eeea3c09
Salman472 <mds619820@gmail.com>
2026-07-12 11:35:30 +0600

    server is running on port 5000

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import { Server } from "node:http";
+import app from "./app.js";
+import env from "./config/env.js";
+
+let server : Server
+
+async function bootstrap() {
+    try {
+        server = app.listen (env.port, ()=>{
+            console.log(`server is running on port ${env.port}`);
+        })
+    } catch (error) {
+        console.error('Failed to start server :', error);
+        process.exit(1)
+    }
+}
+
+(async()=>{
+    await bootstrap()
+})()
