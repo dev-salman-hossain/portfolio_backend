43dcd08baba0db53edc9b246d3f6ee1a8486d436
Salman472 <mds619820@gmail.com>
2026-07-12 11:34:47 +0600

    app created

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, { Application, Request, Response } from "express";
+import cors from 'cors'
+import cookieParser from "cookie-parser";
+import rateLimit from "express-rate-limit";
+import port from './config/env.js'
+
+
+const createApp=()=>{
+    const app:Application = express()
+    app.use(express.json())
+    app.use(cors())
+    app.use(rateLimit)
+}
+
+
+export default createApp
