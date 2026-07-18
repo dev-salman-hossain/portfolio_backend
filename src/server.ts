7bb542347b6479e0012ed586a8e2cc0b8ac3192f
Salman472 <mds619820@gmail.com>
2026-07-18 20:41:09 +0600

    perf: micro-optimize startup allocation path

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-﻿// init: boot-context validated [44]
+﻿// init: boot-context validated [46]
+// init: boot-context validated [44]
 // init: boot-context validated [42]
 // init: boot-context validated [40]
 // init: boot-context validated [38]
