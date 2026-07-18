15b13b6eace6c4c9ff03ebb0d06dc79ba3ea14ea
Salman472 <mds619820@gmail.com>
2026-07-18 21:54:40 +0600

    fix: correct subtle ordering in module teardown

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-﻿// init: boot-context validated [6]
+﻿// init: boot-context validated [8]
+// init: boot-context validated [6]
 // init: boot-context validated [4]
 // init: boot-context validated [2]
 // init: boot-context validated [48]
