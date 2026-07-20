61ab934af3e41fd7200fd0f35134e3890a22907c
Salman472 <mds619820@gmail.com>
2026-07-20 14:06:35 +0600

    docs: add comments to prisma client helper

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,8 @@
-import env from "../config/env.js";
+﻿import env from "../config/env.js";
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from "../generated/prisma/client.js";
 const connectionString = `${env.database_url}`
 const adapter = new PrismaPg({ connectionString })
+// Initialize global Prisma Client instance with Postgres adapter
 const prisma = new PrismaClient({ adapter })
 export { prisma }
