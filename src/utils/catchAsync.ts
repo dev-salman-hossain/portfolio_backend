b51d2ef31123196c16982aa9a808cb87a0d71e57
Salman472 <mds619820@gmail.com>
2026-07-20 07:06:35 +0600

    docs: add JSDoc comments to catchAsync helper

--- src/utils/catchAsync.ts
+++ src/utils/catchAsync.ts
@@ -1,5 +1,9 @@
-import { Request, Response, NextFunction, RequestHandler } from "express";
+﻿import { Request, Response, NextFunction, RequestHandler } from "express";
 
+/**
+ * Wraps an asynchronous Express request handler to catch any rejected promises
+ * and pass them to the next middleware (error handler).
+ */
 const catchAsync = (fn: RequestHandler) => {
     return (req: Request, res: Response, next: NextFunction) => {
         Promise.resolve(fn(req, res, next)).catch((err) => next(err));
