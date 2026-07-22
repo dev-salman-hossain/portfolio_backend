c2d6c9203c461287de818e7f2cfe106ccad57886
Salman472 <mds619820@gmail.com>
2026-07-22 22:07:09 +0600

    feat: implement project database CRUD queries

---
+++ src/modules/project/project.service.ts
@@ -0,0 +1,45 @@
+﻿import { prisma } from "../../lib/prisma.js";
+
+const createProject = async (payload: any) => {
+  const slug = payload.title.toLowerCase().replace(/[^a-z0-9]+/g, "-").replace(/(^-|-$)+/g, "");
+  return await prisma.project.create({
+    data: { ...payload, slug },
+  });
+};
+
+const getAllProjects = async () => {
+  return await prisma.project.findMany({
+    include: { category: true },
+  });
+};
+
+const getProjectById = async (id: string) => {
+  return await prisma.project.findUnique({
+    where: { id },
+    include: { category: true, images: true },
+  });
+};
+
+const updateProject = async (id: string, payload: any) => {
+  if (payload.title) {
+    payload.slug = payload.title.toLowerCase().replace(/[^a-z0-9]+/g, "-").replace(/(^-|-$)+/g, "");
+  }
+  return await prisma.project.update({
+    where: { id },
+    data: payload,
+  });
+};
+
+const deleteProject = async (id: string) => {
+  return await prisma.project.delete({
+    where: { id },
+  });
+};
+
+export const ProjectService = {
+  createProject,
+  getAllProjects,
+  getProjectById,
+  updateProject,
+  deleteProject,
+};
