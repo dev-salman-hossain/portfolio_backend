d660259e34354d249c2091d5540eec603a8b78fa
Salman472 <mds619820@gmail.com>
2026-07-22 23:09:09 +0600

    feat: register endpoints for contact messages

---
+++ src/modules/contact/contact.route.ts
@@ -0,0 +1,13 @@
+﻿import { Router } from "express";
+import validateRequest from "../../middleware/validateRequest.js";
+import isAuthenticated from "../../middleware/authenticate.js";
+import { createContactValidationSchema } from "./contact.validation.js";
+import { ContactController } from "./contact.controller.js";
+
+const router = Router();
+
+router.post("/", validateRequest(createContactValidationSchema), ContactController.saveMessage);
+router.get("/", isAuthenticated, ContactController.getMessages);
+router.delete("/:id", isAuthenticated, ContactController.deleteMessage);
+
+export const ContactRoutes = router;
