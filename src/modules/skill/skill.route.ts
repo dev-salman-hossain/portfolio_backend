531296e105fc14faf61716ac08afdfbe4d4e9d6b
Salman472 <mds619820@gmail.com>
2026-07-22 22:30:09 +0600

    feat: add skill resource routes and middlewares

---
+++ src/modules/skill/skill.route.ts
@@ -0,0 +1,14 @@
+﻿import { Router } from "express";
+import validateRequest from "../../middleware/validateRequest.js";
+import isAuthenticated from "../../middleware/authenticate.js";
+import { createSkillValidationSchema, updateSkillValidationSchema } from "./skill.validation.js";
+import { SkillController } from "./skill.controller.js";
+
+const router = Router();
+
+router.post("/", isAuthenticated, validateRequest(createSkillValidationSchema), SkillController.createSkill);
+router.get("/", SkillController.getAllSkills);
+router.patch("/:id", isAuthenticated, validateRequest(updateSkillValidationSchema), SkillController.updateSkill);
+router.delete("/:id", isAuthenticated, SkillController.deleteSkill);
+
+export const SkillRoutes = router;
