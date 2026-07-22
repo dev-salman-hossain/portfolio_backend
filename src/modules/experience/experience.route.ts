746a3778729e7284db637d851ddc5dd38131d3e0
Salman472 <mds619820@gmail.com>
2026-07-22 22:50:09 +0600

    feat: define endpoints for experience resource

---
+++ src/modules/experience/experience.route.ts
@@ -0,0 +1,14 @@
+﻿import { Router } from "express";
+import validateRequest from "../../middleware/validateRequest.js";
+import isAuthenticated from "../../middleware/authenticate.js";
+import { createExperienceValidationSchema, updateExperienceValidationSchema } from "./experience.validation.js";
+import { ExperienceController } from "./experience.controller.js";
+
+const router = Router();
+
+router.post("/", isAuthenticated, validateRequest(createExperienceValidationSchema), ExperienceController.createExperience);
+router.get("/", ExperienceController.getAllExperiences);
+router.patch("/:id", isAuthenticated, validateRequest(updateExperienceValidationSchema), ExperienceController.updateExperience);
+router.delete("/:id", isAuthenticated, ExperienceController.deleteExperience);
+
+export const ExperienceRoutes = router;
