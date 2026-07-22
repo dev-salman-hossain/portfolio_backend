6d795400d3d8b1f42b36d4a8b745ab83bf627d08
Salman472 <mds619820@gmail.com>
2026-07-22 22:24:09 +0600

    feat: implement skill service CRUD operations

---
+++ src/modules/skill/skill.service.ts
@@ -0,0 +1,31 @@
+﻿import { prisma } from "../../lib/prisma.js";
+
+const createSkill = async (payload: any) => {
+  return await prisma.skills.create({
+    data: payload,
+  });
+};
+
+const getAllSkills = async () => {
+  return await prisma.skills.findMany();
+};
+
+const updateSkill = async (id: string, payload: any) => {
+  return await prisma.skills.update({
+    where: { id },
+    data: payload,
+  });
+};
+
+const deleteSkill = async (id: string) => {
+  return await prisma.skills.delete({
+    where: { id },
+  });
+};
+
+export const SkillService = {
+  createSkill,
+  getAllSkills,
+  updateSkill,
+  deleteSkill,
+};
