ebd94cc9a7a701289eb99b100d2bd6e1c71465e6
Salman472 <mds619820@gmail.com>
2026-07-22 22:58:09 +0600

    feat: define validation schema for contact messages

---
+++ src/modules/contact/contact.validation.ts
@@ -0,0 +1,10 @@
+﻿import { z } from "zod";
+
+export const createContactValidationSchema = z.object({
+  body: z.object({
+    name: z.string({ required_error: "Name is required" }),
+    email: z.string({ required_error: "Email is required" }).email("Invalid email"),
+    subject: z.string({ required_error: "Subject is required" }),
+    message: z.string({ required_error: "Message is required" }),
+  }),
+});
