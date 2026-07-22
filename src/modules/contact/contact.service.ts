7dec412b6efed170f9b144de3397221b85569807
Salman472 <mds619820@gmail.com>
2026-07-22 23:00:09 +0600

    feat: implement contact message storage in DB

---
+++ src/modules/contact/contact.service.ts
@@ -0,0 +1,25 @@
+﻿import { prisma } from "../../lib/prisma.js";
+
+const saveMessage = async (payload: any) => {
+  return await prisma.contactMessage.create({
+    data: payload,
+  });
+};
+
+const getMessages = async () => {
+  return await prisma.contactMessage.findMany({
+    orderBy: { createdAt: "desc" },
+  });
+};
+
+const deleteMessage = async (id: string) => {
+  return await prisma.contactMessage.delete({
+    where: { id },
+  });
+};
+
+export const ContactService = {
+  saveMessage,
+  getMessages,
+  deleteMessage,
+};
