688ee20b557e5f717c4e29966a36e9407b7159d7
Salman472 <mds619820@gmail.com>
2026-07-22 23:05:09 +0600

    feat: add controller handlers for contact forms

---
+++ src/modules/contact/contact.controller.ts
@@ -0,0 +1,41 @@
+﻿import { Request, Response } from "express";
+import catchAsync from "../../utils/catchAsync.js";
+import sendResponse from "../../utils/sendResponse.js";
+import { ContactService } from "./contact.service.js";
+import { StatusCodes } from "http-status-codes";
+
+const saveMessage = catchAsync(async (req: Request, res: Response) => {
+  const result = await ContactService.saveMessage(req.body);
+  sendResponse(res, {
+    statusCode: StatusCodes.CREATED,
+    success: true,
+    message: "Message sent successfully",
+    data: result,
+  });
+});
+
+const getMessages = catchAsync(async (req: Request, res: Response) => {
+  const result = await ContactService.getMessages();
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: "Messages retrieved successfully",
+    data: result,
+  });
+});
+
+const deleteMessage = catchAsync(async (req: Request, res: Response) => {
+  const result = await ContactService.deleteMessage(req.params.id);
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: "Message deleted successfully",
+    data: result,
+  });
+});
+
+export const ContactController = {
+  saveMessage,
+  getMessages,
+  deleteMessage,
+};
