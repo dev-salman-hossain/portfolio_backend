dc1b8acedcaaeb1a7ba8d601b51d63eaf2f629e3
Salman472 <mds619820@gmail.com>
2026-07-22 22:14:09 +0600

    feat: define project API endpoints & routes

---
+++ src/modules/project/project.route.ts
@@ -0,0 +1,15 @@
+﻿import { Router } from "express";
+import validateRequest from "../../middleware/validateRequest.js";
+import isAuthenticated from "../../middleware/authenticate.js";
+import { createProjectValidationSchema, updateProjectValidationSchema } from "./project.validation.js";
+import { ProjectController } from "./project.controller.js";
+
+const router = Router();
+
+router.post("/", isAuthenticated, validateRequest(createProjectValidationSchema), ProjectController.createProject);
+router.get("/", ProjectController.getAllProjects);
+router.get("/:id", ProjectController.getProjectById);
+router.patch("/:id", isAuthenticated, validateRequest(updateProjectValidationSchema), ProjectController.updateProject);
+router.delete("/:id", isAuthenticated, ProjectController.deleteProject);
+
+export const ProjectRoutes = router;
