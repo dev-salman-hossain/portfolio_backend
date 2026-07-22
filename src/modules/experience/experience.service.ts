2a0201dffbc29f27ec487e8da21e8daad4d394cb
Salman472 <mds619820@gmail.com>
2026-07-22 22:41:09 +0600

    feat: implement experience database service

---
+++ src/modules/experience/experience.service.ts
@@ -0,0 +1,31 @@
+﻿import { prisma } from "../../lib/prisma.js";
+
+const createExperience = async (payload: any) => {
+  return await prisma.experience.create({
+    data: payload,
+  });
+};
+
+const getAllExperiences = async () => {
+  return await prisma.experience.findMany();
+};
+
+const updateExperience = async (id: string, payload: any) => {
+  return await prisma.experience.update({
+    where: { id },
+    data: payload,
+  });
+};
+
+const deleteExperience = async (id: string) => {
+  return await prisma.experience.delete({
+    where: { id },
+  });
+};
+
+export const ExperienceService = {
+  createExperience,
+  getAllExperiences,
+  updateExperience,
+  deleteExperience,
+};
